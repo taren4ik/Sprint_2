f91a60d98c44a85391e62bcec4fa2cce261b5926
Dmitry Permyakov <taren4ik@mail.ru>
2022-01-05 23:45:21 +1000

    Правка read_package

--- homework.py
+++ homework.py
@@ -1,7 +1,7 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def _init__(self,
-                training_type: str,
+    def __init__(self,
+                training_type,
                 duration: float,
                 distance: float,
                 speed: float,
@@ -17,8 +17,8 @@ class InfoMessage:
         training_type = self.training_type
         duration = round(self.duration, 3)
         distance = round(self.distance, 3) 
-        speed= round(self.speed, 3)
-        calories= round(self.calories, 3)
+        speed = round(self.speed, 3)
+        calories = round(self.calories, 3)
 
         msg: str = (f'Тип тренировки: {training_type};'
         f'Длительность: {duration} ч.; Дистанция: {distance} км;' 
@@ -56,8 +56,13 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        obj = InfoMessage()
-        return obj.get_message()
+        training_type = type(self).__name__
+        duration = self.duration
+        distance = self.get_distance()
+        speed = self.get_mean_speed()
+        calories = self.get_spent_calories()   
+        return (InfoMessage(training_type, duration, distance, speed, calories)
+        .get_message())
 
 
 class Running(Training):
@@ -66,10 +71,8 @@ class Running(Training):
         run_cal_1: int = 18
         run_cal_2: int = 20
         return ((run_cal_1 * super().get_mean_speed() - run_cal_2) *
-        self.weight / Training.M_IN_KM * self.duration*60)
+        self.weight / Training.M_IN_KM * (self.duration * 60))
  
-get_spent_calories
-
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
     def __init__(self,
@@ -89,21 +92,21 @@ class SportsWalking(Training):
         walc_cal_1: float = 0.035
         walc_cal_2: float = 0.029
         walc_cal_3: int = 2
-        return (walc_cal_1 * self.weight + 
+        return ((walc_cal_1 * self.weight + 
         (super().get_mean_speed()**walc_cal_3 // self.height)
-        * walc_cal_2 * self.weight) * self.duration * 60
+        * walc_cal_2 * self.weight) * self.duration * 60)
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-
+    LEN_STEP = 1.38
     def __init__(self,
                  action: int,
                  duration: float,
                  weight: float,
-                 height: float,
                  length_pool: float,
-                 count_pool: float) -> None:
+                 count_pool: float
+                 ) -> None:
 
         super().__init__(action,
                  duration,
@@ -111,32 +114,33 @@ class Swimming(Training):
                  )
         self.length_pool = length_pool
         self.count_pool = count_pool
-        self.LEN_STEP = 1.38
+        
 
 
     def get_mean_speed(self) -> float:
-        return (self.length_pool * self.count_pool / Training.M_IN_KM / self.duration)
+        return (self.length_pool * self.count_pool / Training.M_IN_KM /
+        self.duration)
  
     def get_spent_calories(self) -> float:
         swim_cal_1: float = 1.1
         swim_cal_2: float = 2
-        return ((super().get_mean_speed() + swim_cal_1) * swim_cal_2 * self.weight)
+        return (self.get_mean_speed() + swim_cal_1) * swim_cal_2 * self.weight
+
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    dic_wtype: dict= {
-    'SWM': 'Swimming',
-    'RUN': 'Running',
-    'WLK': 'SportsWalking'
-    }
-    work_obj: Training =  dic_wtype[workout_type]
-    return work_obj(*data)
+    train_dict: dict= {
+    'SWM': Swimming,
+    'RUN': Running,
+    'WLK': SportsWalking
+    } 
+    return (train_dict[workout_type])(*data) #создаем объект соответ. класса и передаем данные от датчиков
 
 
 def main(training: Training) -> None:
     """Главная функция."""
-    info = training.show_training_info()
-    return print(info.get_message())
+    info = type(training).show_training_info()
+    print (info.get_message())
 
 
 if __name__ == '__main__':
